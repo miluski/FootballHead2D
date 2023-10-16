105db59b938fbb04a8020e3966767e891512cd76
fafusinho <krzysiek10911@gmail.com>
2023-10-16 21:43:55 +0200

    Inicjalizacja w klasie Engine

--- FootballHead2D.cpp
+++ FootballHead2D.cpp
@@ -4,30 +4,32 @@
 using namespace std;
 
 class Engine {
-public:             // Access specifier
-    int myNum;        // Attribute (int variable)
-    string myString;
+public:
+    void initialize() {
+        sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+        sf::CircleShape shape(100.f);
+        shape.setFillColor(sf::Color::Green);
+
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(shape);
+            window.display();
+        }
+    }
 };
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    Engine engine;
+    engine.initialize();
 
     return 0;
 }
