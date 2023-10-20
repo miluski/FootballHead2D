37be829c975fda4ad6a29cf0238f8319c93d3dcf
miluski <sprytnykowal@gmail.com>
2023-10-20 15:25:44 +0200

    Code refactor + clear window/sprite addon + Engine is now singleton

--- Engine.cpp
+++ Engine.cpp
@@ -1,132 +1,139 @@
 ﻿#include "Engine.hpp"
 
-void Engine::startupView() {
-    string startLog = "[INFO] " + getCurrentTime() + " Main menu started ";
-    setLogContent(startLog);
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Menu");
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Blue);
+void Engine::mainWindowSetup() {
+    sf::RenderWindow window(sf::VideoMode(400, 400), "", sf::Style::Titlebar | sf::Style::Close);
+    sf::Clock breakClock;
+    font = *getArialFont();
     window.setFramerateLimit(60);
-    sf::Text text;
-    setSettingsOf(text);
-    sf::Clock clock;
     while (window.isOpen()) {
-        float currentTime = clock.restart().asSeconds();
-        string fpsText = getFramePerSecondText(currentTime);
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        int elapsed = checkTimer();
-        if (elapsed > 5) 
+        serveWindowCloseEvent(window);
+        float elapsed = breakClock.getElapsedTime().asSeconds();
+        if (elapsed > 5)
             break;
-        setStringOf(text, fpsText);
-        window.clear();
-        window.draw(shape);
-        window.draw(text);
+        switch (activeWindowName) {
+            case STARTUP_SETTINGS:
+                settingsWindowSetup(&window);
+            break;
+            case MENU:
+                menuWindowSetup(&window);
+            break;
+            case GAME:
+            break;
+            case GAME_HELPER:
+            break;
+        }
         window.display();
     }
+}
+
+void Engine::serveWindowCloseEvent(sf::RenderWindow& window) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            window.close();
+    }
+}
+
+void Engine::settingsWindowSetup(sf::RenderWindow* window) {
+    window->setTitle("Ustawienia gry");
+    sf::RectangleShape* resolution1Button = getResolutionButton(window->getSize(), 20);
+    sf::RectangleShape* resolution2Button = getResolutionButton(window->getSize(), 80);
+    sf::RectangleShape* resolution3Button = getResolutionButton(window->getSize(), 140);
+    sf::Text* chooseRes = getText("Wybierz rozdzielczość aby rozpocząć");
+    sf::Text* resolution1 = getText("1280 x 720");
+    sf::Text* resolution2 = getText("1366 x 768");
+    sf::Text* resolution3 = getText("1600 x 900");
+    chooseRes->setPosition(140, 110);
+    resolution1->setPosition(140, 130);
+    resolution2->setPosition(140, 190);
+    resolution3->setPosition(140, 250);
+    window->clear();
+    window->draw(*resolution1Button);
+    window->draw(*resolution2Button);
+    window->draw(*resolution3Button);
+    window->draw(*chooseRes);
+    window->draw(*resolution1);
+    window->draw(*resolution2);
+    window->draw(*resolution3);
+}
+
+sf::RectangleShape* Engine::getResolutionButton(sf::Vector2u windowSize, float yShift) {
+    sf::Vector2f centerPoint(windowSize.x / 2, windowSize.y / 2);
+    sf::RectangleShape* resolutionButton = new sf::RectangleShape();
+    sf::FloatRect rectBounds = resolutionButton->getLocalBounds();
+    resolutionButton->setSize(sf::Vector2f(200, 50));
+    resolutionButton->setFillColor(sf::Color::Green);
+    resolutionButton->setPosition(110, (((windowSize.y)/2 - rectBounds.height) / 2)+yShift);
+    return resolutionButton;
+}
+
+void Engine::menuWindowSetup(sf::RenderWindow* window) {
+    setLogContent("[INFO] " + getCurrentTime() + " Main menu started ");
+    sf::Clock fpsClock;
+    sf::Text* fpsTextPlace = getText();
+    float currentTime = fpsClock.restart().asSeconds();
+    string fpsText = getFramePerSecondText(currentTime);
+    fpsTextPlace->setString(fpsText);
+    window->setTitle("Menu");
+    window->setSize(windowSize);
+    window->clear();
+    window->draw(*fpsTextPlace);
+}
+
+void Engine::clearWindowToColor(sf::RenderWindow& window, sf::Color color) {
+    window.clear(color);
+}
+
+void Engine::clearSpriteToColor(sf::Sprite& sprite, sf::Color color) {
+    sprite.setColor(color);
+}
+
+sf::Text* Engine::getText() {
+    sf::Text* text = new sf::Text();
+    text->setFont(font);
+    text->setFillColor(sf::Color::White);
+    text->setCharacterSize(25);
+    return text;
+}
+
+sf::Text* Engine::getText(string content) {
+    sf::Text* text = new sf::Text();
+    text->setFont(font);
+    text->setFillColor(sf::Color::White);
+    text->setString(content);
+    text->setCharacterSize(28);
+    return text;
 }
 
 string Engine::getCurrentTime() {
     auto timeNow = chrono::system_clock::to_time_t(chrono::system_clock::now());
     char timeStringBuffer[50] = "";
-    ctime_s(timeStringBuffer, 27, &timeNow);
+    ctime_s(timeStringBuffer, 50, &timeNow);
     string timeString = timeStringBuffer;
     timeString.erase(remove(timeString.begin(), timeString.end(), '\n'), timeString.cend());
     return timeString;
 }
 
 string Engine::getFramePerSecondText(float currentTime) {
-    float fps = round(1.0f / currentTime);
-    string fpsText = to_string(fps);
-    int decimalPlacePos = fpsText.find(".000000");
+    float fps = 1.0f / currentTime;
+    string fpsText = to_string(round(fps));
+    size_t decimalPlacePos = fpsText.find(".000000");
     fpsText.erase(decimalPlacePos);
-    return fpsText;
+    return "FPS: " + fpsText;
 }
 
-int Engine::checkTimer() {
-    sf::Time elapsed1 = clock.getElapsedTime();
-    return elapsed1.asSeconds();
-}
-
-void Engine::setSettingsOf(sf::Text &text) {
-    sf::Font font = loadArialFont();
-    text.setFont(font);
-    text.setCharacterSize(48);
-    text.setFillColor(sf::Color::White);
-}
-
-void Engine::setStringOf(sf::Text& text, string fpsText) {
-    text.setString("FPS: " + fpsText);
-}
-
-sf::Font Engine::loadArialFont() {
-    sf::Font font;
+sf::Font* Engine::getArialFont() {
+    sf::Font* font = new sf::Font();
     string logContent = "";
-    if (!font.loadFromFile("arial.ttf")) 
+    if (!font->loadFromFile("arial.ttf")) 
         setLogContent("[ERROR] " + getCurrentTime() + " Unsuccessfull arial font load");
     else 
         setLogContent("[INFO] " + getCurrentTime() + " Arial font was successfully loaded");
     return font;
 }
 
-void Engine::openSettings() {
-    sf::RenderWindow window(sf::VideoMode(200, 400), "Opcje uruchamiania");
-    sf::RectangleShape resolution1;
-    sf::RectangleShape resolution2;
-    sf::RectangleShape resolution3;
-    sf::Text chosenResolution;
-    sf::Font font = loadArialFont();
-    sf::Text text1;
-    sf::Text text2;
-    sf::Text text3;
-    resolution1.setSize(sf::Vector2f(160, 50));//dodac tu res 1280x720
-    resolution1.setFillColor(sf::Color(25, 55, 60, 255));
-    resolution1.setPosition(sf::Vector2f(20, 100));
-    text1.setFont(font);
-    text1.setString("1280 x 720");
-    text1.setCharacterSize(28);
-    text1.setFillColor(sf::Color::White);
-    text1.setPosition(sf::Vector2f(20, 100));
-    resolution2.setSize(sf::Vector2f(160, 50));//dodac tu res 1366x768
-    resolution2.setFillColor(sf::Color::Green);
-    resolution2.setPosition(sf::Vector2f(20, 180));
-    text2.setFont(font);
-    text2.setString("1366 x 768");
-    text2.setCharacterSize(28);
-    text2.setFillColor(sf::Color::White);
-    text2.setPosition(sf::Vector2f(20, 180));
-    resolution3.setSize(sf::Vector2f(160, 50));//dodac tu res 1600x900
-    resolution3.setFillColor(sf::Color::Green);
-    resolution3.setPosition(sf::Vector2f(20, 260));
-    text3.setFont(font);
-    text3.setString("1600 x 900");
-    text3.setCharacterSize(28);
-    text3.setFillColor(sf::Color::White);
-    text3.setPosition(sf::Vector2f(20, 260));
-    //Dodać tytuł gry
-    //Dodać przycisk graj + wywołanie engine.startupView na evencie onClick
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-                startupView();
-            }
-        }
-        window.clear();
-        window.draw(resolution1);
-        window.draw(resolution2);
-        window.draw(resolution3);
-        window.display();
-    }
-}
-
 void Engine::setLogContent(string logContent) {
     this->logContent = logContent;
-    this->logFilePath = "logs.log";
     if (!validateLogContentFormat()) {
         string error = "[ERROR] " + getCurrentTime() + " Uncorrect log format";
         this->logContent = error;
@@ -136,19 +143,18 @@ void Engine::setLogContent(string logContent) {
 
 void Engine::saveLog() {
     fstream fileStream;
-    fileStream.open(logFilePath, ios::app);
+    fileStream.open("logs.log", ios::app);
     fileStream << logContent << endl;
     fileStream.close();
+    cout << logContent << endl;
 }
 
 bool Engine::validateLogContentFormat() {
-    int isValidErrorStart = this->logContent.find("[ERROR]");
-    int isValidInfoStart = this->logContent.find("[INFO]");
-    int isValidDebugStart = this->logContent.find("[DEBUG]");
-    if (isValidErrorStart != string::npos || isValidInfoStart != string::npos || isValidDebugStart != string::npos) {
-        cout << this->logContent << endl;
+    size_t isValidErrorStart = this->logContent.find("[ERROR]");
+    size_t isValidInfoStart = this->logContent.find("[INFO]");
+    size_t isValidDebugStart = this->logContent.find("[DEBUG]");
+    if (isValidErrorStart != string::npos || isValidInfoStart != string::npos || isValidDebugStart != string::npos) 
         return true;
-    }
     else
         return false;
 }
@@ -158,7 +164,7 @@ bool Engine::validateLogContentFormat() {
 */
 
 int main() {
-    Engine engine;
-    engine.openSettings();
+    Engine::getInstance().activeWindowName = STARTUP_SETTINGS;
+    Engine::getInstance().mainWindowSetup();
     return 0;
 }
--- Engine.hpp
+++ Engine.hpp
@@ -11,23 +11,33 @@
 
 using namespace std;
 
+enum windowNames {STARTUP_SETTINGS=0, MENU=1, GAME=2, GAME_HELPER=3};
+
 class Engine {
 public:
-    void openSettings();
+    int activeWindowName;
+    static Engine& getInstance() {
+        static Engine engine;
+        return engine;
+    }
+    void mainWindowSetup();
+    void clearWindowToColor(sf::RenderWindow& window, sf::Color color);
+    void clearSpriteToColor(sf::Sprite& sprite, sf::Color color);
 private:
     sf::Clock clock;
+    sf::Font font;
+    sf::Vector2u windowSize;
     string logContent;
-    string logFilePath;
-    void startupView();
+    void settingsWindowSetup(sf::RenderWindow*);
+    void menuWindowSetup(sf::RenderWindow*);
     string getCurrentTime();
     string getFramePerSecondText(float);
-    int checkTimer();
-    void setSettingsOf(sf::Text &);
-    void setStringOf(sf::Text&, string);
-    sf::Font loadArialFont();
-    void installMouse();
-    void installKeyboard();
+    sf::Font* getArialFont();
+    sf::Text* getText();
+    sf::Text* getText(string content);
+    sf::RectangleShape* getResolutionButton(sf::Vector2u windowSize, float);
     bool validateLogContentFormat();
     void setLogContent(string);
     void saveLog();
+    void serveWindowCloseEvent(sf::RenderWindow&);
 };
