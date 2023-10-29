1a84d3eae5bc72b8b7265e6a7375ca60c9f1a640
miluski <sprytnykowal@gmail.com>
2023-10-29 17:51:03 +0100

    LineSegment, Point2D classes full implementation, PrimitiveRenderer 90% implementation

--- Engine.cpp
+++ Engine.cpp
@@ -1,68 +1,179 @@
 ﻿#include "Engine.hpp"
 
 void Engine::mainWindowSetup() {
-    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "", sf::Style::Titlebar | sf::Style::Close);
-    sf::Text* fpsTextPlace = getText();
-    sf::Clock fpsClock;
+    Clock fpsClock;
     font = *getArialFont();
-    window.setFramerateLimit(60);
-    while (window.isOpen()) {
+    while (window->isOpen()) {
         float elapsed = breakClock.getElapsedTime().asSeconds();
-        if (elapsed > 5)
-            window.close();
-        serveWindowCloseEvent(window);
         float currentTime = fpsClock.restart().asSeconds();
-        string fpsText = getFramePerSecondText(currentTime);
+        if (elapsed > 300)
+            window->close();
+        serveWindowCloseEvent();
         switch (activeWindowName) {
             case STARTUP_SETTINGS:
-                settingsWindowSetup(&window);
+                settingsWindowSetup();
             break;
             case MENU:
-                menuWindowSetup(&window, *fpsTextPlace, fpsText);
+                menuWindowSetup();
             break;
             case GAME:
-                handleBuffers(&window);
+                gameWindowSetup(currentTime);
             break;
             case GAME_HELPER:
+                gameHelperWindowSetup();
             break;
         }
-        window.display();
+        window->display();
     }
 }
 
-void Engine::setSecondBufferTexture(sf::Texture bufferTexture) {
-    secondBuffer->clear();
-    secondBuffer->draw(sf::Sprite(bufferTexture));
-    secondBuffer->display();
+void Engine::settingsWindowSetup() {
+    window->setTitle("Ustawienia gry");
+    window->clear();
+    drawButtons();
+    drawTexts();
 }
 
-void Engine::handleBuffers(sf::RenderWindow* window) {
-    if (activeBuffer == "main") {
-        mainBuffer->clear();
-        mainBuffer->draw(sf::Sprite(secondBuffer->getTexture()));
-        mainBuffer->display();
-        window->draw(sf::Sprite(mainBuffer->getTexture()));
-        activeBuffer = "second";
+void Engine::menuWindowSetup() {
+    VideoMode desktop = VideoMode::getDesktopMode();
+    setFrameRateLimit();
+
+    /* Testy PrimitiveRenderer 
+        PrimitiveRenderer primitiveRenderer;
+        primitiveRenderer.drawRectangle(Vector2f(200.0f, 200.0f), Vector2f(100.0f, 100.0f), Color::White);
+        primitiveRenderer.drawCircle(50.0f, Vector2f(100.0f, 100.0f), Color::White);
+        primitiveRenderer.drawTriangle(25.0f, Vector2f(100.0f, 100.0f), Color::White);
+        primitiveRenderer.drawIncreasedLine(Vector2f(0.0f, 0.0f), Vector2f(100.0f, 100.0f), Color::Blue); // m <=1
+        primitiveRenderer.drawIncreasedLine(Vector2f(0.0f, 0.0f), Vector2f(100.0f, 200.0f), Color::Blue); // m przekracza 1
+        primitiveRenderer.drawLine(Vector2f(0.0f, 0.0f), Vector2f(100.0f, 100.0f), Color::Blue); // zgadza się z m <=1 (linia renderowana przez mechanizm SFML)
+        primitiveRenderer.drawLine(Vector2f(0.0f, 0.0f), Vector2f(100.0f, 200.0f), Color::Blue); // zgadza się z m przekracza 1 (linia renderowana przez mechanizm SFML)
+     Koniec testów */
+
+    /* Testy Point2D 
+        Point2D point2D;
+        point2D.setCoords(100.0f, 100.0f);
+        point2D.drawPoint();
+     Koniec testów */
+
+    /* Testy LineSegment 
+        Point2D startPoint;
+        Point2D endPoint;
+        startPoint.setCoords(0.0f, 0.0f);
+        endPoint.setCoords(100.0f, 100.0f);
+        LineSegment lineSegment;
+        lineSegment.setStartPoint(startPoint);
+        lineSegment.setEndPoint(endPoint);
+        lineSegment.drawLine();
+     Koniec testów */
+
+    if (!centered) {
+        window->setPosition(Vector2i((desktop.width - windowSize->x) / 2, (desktop.height - windowSize->y) / 2));
+        centered = true;
     }
-    else {
-        secondBuffer->clear();
-        secondBuffer->draw(sf::Sprite(mainBuffer->getTexture()));
-        secondBuffer->display();
-        window->draw(sf::Sprite(secondBuffer->getTexture()));
-        activeBuffer = "main";
+    window->setTitle("Menu");
+    window->setSize(*windowSize);
+}
+
+void Engine::gameWindowSetup(float currentTime) {
+    VideoMode desktop = VideoMode::getDesktopMode();
+    string fpsText = getFramePerSecondText(currentTime);
+    Text* fpsTextPlace = getText();
+    fpsTextPlace->setString(fpsText);
+    Texture* fpsTexture = createTextureFrom(fpsTextPlace, Vector2i(50, 50), Color(58, 157, 35));
+    Sprite bufferTextureSprite;
+    bufferTextureSprite.setTexture(*fpsTexture);
+    setFrameRateLimit();
+    if (!centered) {
+        window->setPosition(Vector2i((desktop.width - windowSize->x) / 2, (desktop.height - windowSize->y) / 2));
+        centered = true;
+    }
+    window->setTitle("FootballHead2D");
+    window->setSize(*windowSize);
+    setSecondBufferTexture(&bufferTextureSprite, Vector2u(50, 50));
+    handleBuffers();
+}
+
+void Engine::gameHelperWindowSetup() {
+    VideoMode desktop = VideoMode::getDesktopMode();
+    setFrameRateLimit();
+    if (!centered) {
+        window->setPosition(Vector2i((desktop.width - windowSize->x) / 2, (desktop.height - windowSize->y) / 2));
+        centered = true;
+    }
+    window->setTitle("Game Helper");
+    window->setSize(*windowSize);
+}
+
+void Engine::setWidthAndHeight(RectangleShape* resolution1Button, RectangleShape* resolution2Button,
+    RectangleShape* resolution3Button) {
+    if (resolution1Button->getFillColor() == Color::Red) {
+        windowSize->x = 1280;
+        windowSize->y = 720;
+        Engine::getInstance().activeWindowName = MENU;
+    }
+    else if (resolution2Button->getFillColor() == Color::Red) {
+        windowSize->x = 1366;
+        windowSize->y = 768;
+        Engine::getInstance().activeWindowName = MENU;
+    }
+    else if (resolution3Button->getFillColor() == Color::Red) {
+        windowSize->x = 1600;
+        windowSize->y = 900;
+        Engine::getInstance().activeWindowName = MENU;
     }
 }
 
-sf::Text* Engine::getText() {
-    sf::Text* text = new sf::Text();
+void Engine::setSecondBufferTexture(Sprite* textureSprite, Vector2u textureSize) {
+    Vector2u windowSize = window->getSize();
+    secondBuffer.create(windowSize.x, windowSize.y);
+    secondBuffer.draw(*textureSprite);
+    secondBuffer.display();
+}
+
+void Engine::setFrameRateLimit() {
+    window->setFramerateLimit(60);
+}
+
+void Engine::setLogContent(string logContent) {
+    this->logContent = logContent;
+    if (!validateLogContentFormat()) {
+        string error = "[ERROR] " + getCurrentTime() + " Uncorrect log format";
+        this->logContent = error;
+    }
+    saveLog();
+}
+
+RenderWindow* Engine::getMainWindow() {
+    return window;
+}
+
+Text* Engine::getText() {
+    Text* text = new Text();
     text->setFont(font);
-    text->setFillColor(sf::Color::White);
+    text->setFillColor(Color::White);
     text->setCharacterSize(15);
     return text;
 }
 
-sf::Font* Engine::getArialFont() {
-    sf::Font* font = new sf::Font();
+Text* Engine::getText(string content) {
+    Text* text = new Text();
+    text->setFont(font);
+    text->setFillColor(Color::White);
+    text->setString(content);
+    text->setCharacterSize(28);
+    return text;
+}
+
+RectangleShape* Engine::getButton(float yShift, Color color) {
+    RectangleShape* button = new RectangleShape();
+    button->setSize(Vector2f(200, 50));
+    button->setFillColor(color);
+    button->setPosition(100.0f, 40.0f + yShift);
+    return button;
+}
+
+Font* Engine::getArialFont() {
+    Font* font = new Font();
     string logContent = "";
     if (!font->loadFromFile("arial.ttf"))
         setLogContent("[ERROR] " + getCurrentTime() + " Unsuccessfull arial font load");
@@ -71,75 +182,30 @@ sf::Font* Engine::getArialFont() {
     return font;
 }
 
-void Engine::serveWindowCloseEvent(sf::RenderWindow& window) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed)
-            window.close();
-        if (event.type == sf::Event::MouseMoved)
-            breakClock.restart();
-    }
-}
-
 string Engine::getFramePerSecondText(float currentTime) {
     float fps = 1.0f / currentTime;
     string fpsText = to_string(round(fps));
     size_t decimalPlacePos = fpsText.find(".000000");
     fpsText.erase(decimalPlacePos);
-    return "FPS: " + fpsText;
+    return fpsText;
 }
 
-void Engine::settingsWindowSetup(sf::RenderWindow* window) {
-    window->setTitle("Ustawienia gry");
-    window->clear();
-    drawButtonsAt(window);
-    drawTextsAt(window);
+string Engine::getCurrentTime() {
+    auto timeNow = chrono::system_clock::to_time_t(chrono::system_clock::now());
+    char timeStringBuffer[50] = "";
+    ctime_s(timeStringBuffer, 50, &timeNow);
+    string timeString = timeStringBuffer;
+    timeString.erase(remove(timeString.begin(), timeString.end(), '\n'), timeString.cend());
+    return timeString;
 }
 
-void Engine::drawButtonsAt(sf::RenderWindow* window) {
-    sf::RectangleShape* playButton = getButton(260, sf::Color::Red);
-    int rectName = getRectNameWhenMouseIsPressedIn(window);
-    switch (rectName) {
-        case RESOLUTION_1:
-            resolution1Button = getButton(20, sf::Color::Red);
-            resolution2Button = getButton(100, sf::Color::Green);
-            resolution3Button = getButton(180, sf::Color::Green);
-        break;
-        case RESOLUTION_2:
-            resolution1Button = getButton(20, sf::Color::Green);
-            resolution2Button = getButton(100, sf::Color::Red);
-            resolution3Button = getButton(180, sf::Color::Green);
-        break;
-        case RESOLUTION_3:
-            resolution1Button = getButton(20, sf::Color::Green);
-            resolution2Button = getButton(100, sf::Color::Green);
-            resolution3Button = getButton(180, sf::Color::Red);
-        break;
-        case PLAY:
-            setWidthAndHeight(resolution1Button, resolution2Button, resolution3Button);
-        break;
-    }
-    window->draw(*resolution1Button);
-    window->draw(*resolution2Button);
-    window->draw(*resolution3Button);
-    window->draw(*playButton);
-}
-
-sf::RectangleShape* Engine::getButton(float yShift, sf::Color color) {
-    sf::RectangleShape* button = new sf::RectangleShape();
-    button->setSize(sf::Vector2f(200, 50));
-    button->setFillColor(color);
-    button->setPosition(100, 40 + yShift);
-    return button;
-}
-
-int Engine::getRectNameWhenMouseIsPressedIn(sf::RenderWindow* window) {
-    sf::IntRect rect1(100, 60, 200, 50);
-    sf::IntRect rect2(100, 140, 200, 50);
-    sf::IntRect rect3(100, 220, 200, 50);
-    sf::IntRect rect4(100, 300, 200, 50);
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-        sf::Vector2i mouseBounds = sf::Mouse::getPosition(*window);
+int Engine::getRectNameWhenMouseIsPressedIn() {
+    IntRect rect1(100, 60, 200, 50);
+    IntRect rect2(100, 140, 200, 50);
+    IntRect rect3(100, 220, 200, 50);
+    IntRect rect4(100, 300, 200, 50);
+    if (Mouse::isButtonPressed(Mouse::Left)) {
+        Vector2i mouseBounds = Mouse::getPosition(*window);
         int mouseX = mouseBounds.x;
         int mouseY = mouseBounds.y;
         if (rect1.contains(mouseX, mouseY))
@@ -154,30 +220,66 @@ int Engine::getRectNameWhenMouseIsPressedIn(sf::RenderWindow* window) {
     return -1;
 }
 
-void Engine::setWidthAndHeight(sf::RectangleShape* resolution1Button, sf::RectangleShape* resolution2Button,
-    sf::RectangleShape* resolution3Button) {
-    if (resolution1Button->getFillColor() == sf::Color::Red) {
-        windowSize.x = 1280;
-        windowSize.y = 720;
-        Engine::getInstance().activeWindowName = MENU;
-    }
-    else if (resolution2Button->getFillColor() == sf::Color::Red) {
-        windowSize.x = 1366;
-        windowSize.y = 768;
-        Engine::getInstance().activeWindowName = MENU;
-    }
-    else if (resolution3Button->getFillColor() == sf::Color::Red) {
-        windowSize.x = 1600;
-        windowSize.y = 900;
-        Engine::getInstance().activeWindowName = MENU;
+Texture* Engine::createTextureFrom(Text* text, Vector2i size, Color textureColor) {
+    RenderTexture renderTexture;
+    renderTexture.create(size.x, size.y);
+    renderTexture.clear(textureColor);
+    renderTexture.draw(*text);
+    renderTexture.display();
+    Texture* texture = new Texture(renderTexture.getTexture());
+    return texture;
+}
+
+Texture* Engine::createTextureFrom(Image* image, Vector2i size) {
+    Texture* texture = new Texture();
+    texture->loadFromImage(*image);
+    return texture;
+}
+
+void Engine::serveWindowCloseEvent() {
+    Event event;
+    while (window->pollEvent(event)) {
+        if (event.type == Event::Closed)
+            window->close();
+        if (event.type == Event::MouseMoved || event.type == Event::MouseButtonPressed || event.type == Event::KeyPressed)
+            breakClock.restart();
     }
 }
 
-void Engine::drawTextsAt(sf::RenderWindow* window) {
-    sf::Text* resolution1 = getText("1280 x 720");
-    sf::Text* resolution2 = getText("1366 x 768");
-    sf::Text* resolution3 = getText("1600 x 900");
-    sf::Text* startGame = getText("Uruchom gre");
+void Engine::drawButtons() {
+    RectangleShape* playButton = getButton(260, Color::Red);
+    int rectName = getRectNameWhenMouseIsPressedIn();
+    switch (rectName) {
+        case RESOLUTION_1:
+            resolution1Button = getButton(20, Color::Red);
+            resolution2Button = getButton(100, Color::Green);
+            resolution3Button = getButton(180, Color::Green);
+        break;
+        case RESOLUTION_2:
+            resolution1Button = getButton(20, Color::Green);
+            resolution2Button = getButton(100, Color::Red);
+            resolution3Button = getButton(180, Color::Green);
+        break;
+        case RESOLUTION_3:
+            resolution1Button = getButton(20, Color::Green);
+            resolution2Button = getButton(100, Color::Green);
+            resolution3Button = getButton(180, Color::Red);
+        break;
+        case PLAY:
+            setWidthAndHeight(resolution1Button, resolution2Button, resolution3Button);
+        break;
+    }
+    window->draw(*resolution1Button);
+    window->draw(*resolution2Button);
+    window->draw(*resolution3Button);
+    window->draw(*playButton);
+}
+
+void Engine::drawTexts() {
+    Text* resolution1 = getText("1280 x 720");
+    Text* resolution2 = getText("1366 x 768");
+    Text* resolution3 = getText("1600 x 900");
+    Text* startGame = getText("Uruchom gre");
     resolution1->setPosition(130, 70);
     resolution2->setPosition(130, 150);
     resolution3->setPosition(130, 230);
@@ -188,40 +290,29 @@ void Engine::drawTextsAt(sf::RenderWindow* window) {
     window->draw(*startGame);
 }
 
-sf::Text* Engine::getText(string content) {
-    sf::Text* text = new sf::Text();
-    text->setFont(font);
-    text->setFillColor(sf::Color::White);
-    text->setString(content);
-    text->setCharacterSize(28);
-    return text;
-}
-
-void Engine::menuWindowSetup(sf::RenderWindow* window, sf::Text fpsTextPlace, string fpsText) {
-    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    fpsTextPlace.setString(fpsText);
-    window->setTitle("Menu");
-    window->setSize(windowSize);
-    window->setPosition(sf::Vector2i((desktop.width - windowSize.x) / 2, (desktop.height - windowSize.y) / 2));
-    window->clear();
-    window->draw(fpsTextPlace);
-}
-
-void Engine::clearWindowToColor(sf::RenderWindow& window, sf::Color color) {
-    window.clear(color);
-}
-
-void Engine::clearSpriteToColor(sf::Sprite& sprite, sf::Color color) {
-    sprite.setColor(color);
-}
-
-void Engine::setLogContent(string logContent) {
-    this->logContent = logContent;
-    if (!validateLogContentFormat()) {
-        string error = "[ERROR] " + getCurrentTime() + " Uncorrect log format";
-        this->logContent = error;
+void Engine::handleBuffers() {
+    if (activeBuffer == "main") {
+        mainBuffer.draw(Sprite(secondBuffer.getTexture()));
+        mainBuffer.display();
+        window->clear(Color(58, 157, 35));
+        window->draw(Sprite(mainBuffer.getTexture()));
+        activeBuffer = "second";
     }
-    saveLog();
+    else {
+        secondBuffer.draw(Sprite(mainBuffer.getTexture()));
+        secondBuffer.display();
+        window->clear(Color(58, 157, 35));
+        window->draw(Sprite(secondBuffer.getTexture()));
+        activeBuffer = "main";
+    }
+}
+
+void Engine::clearWindowToColor(Color color) {
+    window->clear(color);
+}
+
+void Engine::clearSpriteToColor(Sprite& sprite, Color color) {
+    sprite.setColor(color);
 }
 
 bool Engine::validateLogContentFormat() {
@@ -234,15 +325,6 @@ bool Engine::validateLogContentFormat() {
         return false;
 }
 
-string Engine::getCurrentTime() {
-    auto timeNow = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    char timeStringBuffer[50] = "";
-    ctime_s(timeStringBuffer, 50, &timeNow);
-    string timeString = timeStringBuffer;
-    timeString.erase(remove(timeString.begin(), timeString.end(), '\n'), timeString.cend());
-    return timeString;
-}
-
 void Engine::saveLog() {
     fstream fileStream;
     fileStream.open("logs.log", ios::app);
@@ -253,8 +335,6 @@ void Engine::saveLog() {
 
 int main() {
     Engine::getInstance().activeWindowName = STARTUP_SETTINGS;
-    Engine::getInstance().windowSize.x = 400;
-    Engine::getInstance().windowSize.y = 400;
     Engine::getInstance().mainWindowSetup();
     return 0;
 }
